6fa8b207ccc2bb3ec6580a1ea286f56649eae7e4
yBetancurr4002 <ybetancurr4002@example.com>
2025-04-15 11:29:07 -0500

    feat: [ ] credu with sequelize

--- services/user.service.js
+++ services/user.service.js
@@ -1,45 +1,57 @@
 const boom = require('@hapi/boom');
-const connection = require('../libs/postgres.config');
+// const connection = require('../libs/postgres.config');
+const { models } = require('../libs/sequelize.config');
+
 
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    if(!data) {
+      throw boom.badRequest('No data entered');
+    }
+    const newUser = await models.User.create(data);
+
+    return newUser;
   }
 
   async find() {
-    const client = await connection();
 
     try {
-      const response = await client.query('SELECT * FROM tasks.tks_tasks');
+      const response = await models.User.findAll();
 
       return {
-        count: response.rowCount,
-        result: response.rows,
+        count: response.length,
+        result: response,
       };
     } catch (error) {
       console.error('❌ Error ejecutando query en tks_tasks:', error);
       throw error;
-    } finally {
-      client.end(); // ¡Importante para cerrar la conexión!
     }
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+    return user;
   }
 
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+  async update(id, newData) {
+    const user = await this.findOne(id);
+
+    const response = await user.update(newData);
+    return response;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+
+    await user.destroy();
     return { id };
   }
+
 }
 
 module.exports = UserService;
